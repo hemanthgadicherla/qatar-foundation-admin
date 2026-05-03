b0744c30820d75f44ff6ca306b143c44cabe32fb
hemanthgadicherla <hemanthgadicherla26@gmail.com>
2026-05-03 17:04:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -38,8 +38,11 @@ def login_page():
 
 init_routes(app)
 
+import os
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
